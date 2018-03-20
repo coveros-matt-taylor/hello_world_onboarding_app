a033b224dcb7df7fa080490c7f492940ac3f8242
matt.taylor <matthew.taylor@coveros.com>
2018-03-20 14:50:47 -0400

    add odd number test case

--- src/test/java/com/coveros/hello_world_tests/EvenNumberGeneratorTest.java
+++ src/test/java/com/coveros/hello_world_tests/EvenNumberGeneratorTest.java
@@ -6,10 +6,15 @@ import com.coveros.hello_world.EvenNumberGenerator;
 
 public class EvenNumberGeneratorTest {
  @Test
- public void testRandomEvenNumberGenerator() {
-
-  int randomResult = EvenNumberGenerator.generateRandomEven();
-
-  assertEquals(0, randomResult % 2);
- }
+ 	public void testRandomEvenNumberGenerator() {
+	
+	  int randomResult = EvenNumberGenerator.generateRandomEven();
+	
+	  assertEquals(0, randomResult % 2);
+ 	}
+ 	public void testRandomOddNumberGenerator() {
+		
+		int randomResult = EvenNumberGenerator.generateRandomOdd();
+		assertEquals(1, randomResult % 2);
+	}
 }
